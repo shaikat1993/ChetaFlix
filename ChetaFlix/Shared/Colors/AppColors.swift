6333d2937f117b7ebfd941e2468cffb3d3d54598
Md. Sadidur Rahman <mdsadidurrahman74@gmail.com>
2025-06-13 23:32:42 +0300

    feat: implement app color scheme with new color assets and AppColors struct

---
+++ ChetaFlix/Shared/Colors/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  ChetaFlix
+//
+//  Created by Shaikat on 13.6.2025.
+//
+
+import SwiftUI
+
+struct AppColors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppColors()
+}
